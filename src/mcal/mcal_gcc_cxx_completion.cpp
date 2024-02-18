ad822fdd0b0eda768b73262a3d3aa022e71af346
Christopher Kormanyos <ckormanyos@yahoo.com>
2024-02-18 10:20:36 +0100

    Update GCC13 and refine flags and more

--- src/mcal/mcal_gcc_cxx_completion.cpp
+++ src/mcal/mcal_gcc_cxx_completion.cpp
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-//  Copyright Christopher Kormanyos 2007 - 2023.
+//  Copyright Christopher Kormanyos 2007 - 2024.
 //  Distributed under the Boost Software License,
 //  Version 1.0. (See accompanying file LICENSE_1_0.txt
 //  or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -24,13 +24,20 @@ namespace std
 }
 
 void  operator delete(void*)        noexcept;
-void  operator delete(void*, void*) noexcept;
+#if (defined(__GNUC__) && (__GNUC__ > 12))
+inline void operator delete(void*, void*) noexcept;
+#else
+       void operator delete(void*, void*) noexcept;
+#endif
 #if(__cplusplus >= 201400L)
 void operator delete(void*, std::size_t) noexcept;
 #endif
 
 void operator delete(void*)              noexcept { }
+#if (defined(__GNUC__) && (__GNUC__ > 12))
+#else
 void operator delete(void*, void*)       noexcept { }
+#endif
 #if(__cplusplus >= 201400L)
 void operator delete(void*, std::size_t) noexcept { }
 #endif
@@ -43,13 +50,13 @@ extern "C"
   // Also provide stubbed copies of certain empirically found library functions
   // and objects.
 
-  int         atexit              (void (*)(void)) noexcept;
+  int         atexit              (void (*)(void));
   void        __cxa_pure_virtual  (void);
   char*       __cxa_demangle      (const char*, char*, size_t*, int*);
 
   // Implementations of patched functions.
 
-  int         atexit              (void (*)()) noexcept          { return 0; }
+  int         atexit              (void (*)(void))                    { return 0; }
   void        __cxa_pure_virtual  (void)                              { }
   char*       __cxa_demangle      (const char*, char*, size_t*, int*) { return nullptr; }
 
@@ -61,6 +68,7 @@ extern "C"
   const char*  const __env[1U]       = { nullptr };
   const char** const environ         = { nullptr };
 
-  int          __errno;
+  extern int* __errno(void);
+  int* __errno(void) { return nullptr; }
   std::uint8_t __fdlib_version;
 }
