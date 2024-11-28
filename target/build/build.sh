8d2b2b80c267ba35d20ad5eabbe5b8d20fcae170
ckormanyos <ckormanyos@yahoo.com>
2024-11-28 15:29:28 +0100

    Update more syntax

--- target/build/build.sh
+++ target/build/build.sh
@@ -6,7 +6,6 @@
 #  or copy at http://www.boost.org/LICENSE_1_0.txt)
 #
 
-
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #
 # Description of the build options used in the build command below.
